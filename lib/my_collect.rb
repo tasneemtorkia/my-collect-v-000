d60963b98659b148cd85060b41bf5c44f2e7a8b1
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-19 19:10:35 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(languages)
   i = 0 
   full_array = [ ]
   while i < languages.length 
-  full_array << yield(languages[i]).upcase
+  full_array << yield(languages[i])
   i += 1
 end 
 full_array
