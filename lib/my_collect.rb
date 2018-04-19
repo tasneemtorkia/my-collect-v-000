ff566ec69ae215ae09c4cbec39a478e83a80e788
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-19 19:11:05 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(empty_array)
   i = 0 
   full_array = [ ]
   while i < empty_array.length 
-  full_array << yield(languages[i])
+  full_array << yield(empty_array[i])
   i += 1
 end 
 full_array
